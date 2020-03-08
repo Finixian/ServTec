55916f398b9e6605d7fcb59aa87d2ba599692d43
Acevedo Cardona Adelaid Lesdeymariet <adelaid.acevedo16@tectijuana.edu.mx>
2020-03-07 20:30:46 -0800

    Add files via upload

---
+++ Models/Dao/aspirante/aspiranteSql.php
@@ -0,0 +1,25 @@
+<?php
+namespace Model\Dao\Aspirante;
+
+class AspiranteSQL
+{    
+    public static function  registrarAspirante(){
+        $query="insert into aspirante(nombre,paterno,materno,fnacimiento,folio,password,correo,status) values(?,?,?,?,?,?,?,?);";
+        return $query;
+    }
+    
+    public static function actualizarAspirante(){
+        $query="update aspirante set nombre=?, paterno=?, materno=?, fnacimiento=?, folio=?, password=?, correo=? where idAspirante=?;";
+        return $query;
+    }
+    
+    public static function eliminarAspirante(){
+        $query="delete from aspirante where idAspirante=?;";
+        return $query;
+    }
+    
+    public static function traerDatosIdSql(){
+        $query="SELECT * FROM aspirante where idAspirante=?;";
+        return $query;
+    }
+}
