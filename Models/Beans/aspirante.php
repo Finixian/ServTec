6746c14dba94b1cef3b6fc27c7091d2c6abfda51
Acevedo Cardona Adelaid Lesdeymariet <adelaid.acevedo16@tectijuana.edu.mx>
2020-03-07 20:25:57 -0800

    Add files via upload

---
+++ Models/Beans/aspirante.php
@@ -0,0 +1,28 @@
+<?php
+namespace Model\Beans;
+
+class Aspirante {
+    
+    var $id;
+    var $nombre;
+    var $apaterno;
+    var $amaterno;
+    var $fnacimiento;
+    var $folio;
+    var $clave;
+    var $correo;
+    var $tipo;        
+
+    private $data;
+    
+    public function __set($name, $value) {
+        $this->data[$name] = $value;    
+    }
+
+    public function __get($name) {
+        if(array_key_exists($name, $this->data)){
+            return $this->data[$name];
+            return null;
+        }
+    }
+}
---
+++ Models/Beans/formulario.php
@@ -0,0 +1,19 @@
+<?php
+namespace Model\Beans;
+
+class Formulario {
+
+    private $data;
+
+    public function _set($name, $value) {
+        $this->data[$name] = $value;
+    }
+
+    public function _get($name) {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+            return null;
+        }
+    }
+
+}
